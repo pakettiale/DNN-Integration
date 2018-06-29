dba472d0a5a58b6efddcf1446eb7a31065e35dba
Aleksanteri Paakkinen <aleksanteri.paakkinen@gmail.com>
2018-06-29 16:27:47 +0200

    Losses factored into seperate file

--- losses.py
+++ losses.py
@@ -17,12 +17,16 @@ def regression_loss(x_true, x_pred): #x_true <- f(x), x_pred <- h(G(z))
 
 # G_z = generative,       h_G_z = generative_then_regression
 # z   = generative input, Int_f = integral of exp(h)?
-def generator_loss(G_z, z, h_G_z, Int_f, p_z): #x_true = exp(h_z), x_pred <- G_z, x_in = z,
+def generator_loss(G_z, z, h_G_z, Int_f, Int_g, p_z): #x_true = exp(h_z), x_pred <- G_z, x_in = z,
+    Int_g = Int_g
     dim = tf.shape(z)[1]
-    print("dim: ", dim)
-    N = 5120.0# tf.cast(tf.shape(z)[0], tf.float32)
+    N = tf.cast(tf.shape(z)[0], tf.float32)
     slogDJ = tf.reshape(logdet(jacobian(G_z, z)), (-1, 1))
     #print(N, p_z, slogDJ)
-    D = tf.log(2.0) +  1.0/N*tf.reduce_sum(tf.log(p_z) - slogDJ - tf.log(p_z/tf.exp(slogDJ)+tf.exp(h_G_z)/Int_f),(0,)) #
-    #D = -1.0/N*tf.reduce_sum(tf.exp(h_G_z)*(tf.log(tf.exp(h_G_z)/Int_f) - tf.log(p_z) + slogDJ), (0,)) #
+    #D = 1/N*tf.reduce_sum(-tf.log(tf.exp(h_G_z)/Int_f) + tf.log(p_z) - slogDJ, (0,))
+    D = tf.log(2.0) +  1.0/N*tf.reduce_sum((tf.log(p_z) - slogDJ - tf.log(p_z/tf.exp(slogDJ)+h_G_z/Int_f)),(0,)) #
     return D
+
+
+def integral_loss(integral):
+    return (1 - integral)**2
