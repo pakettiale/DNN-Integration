ecd17ab4790fb4e1bd6b9020762b89947f2ac455
Aleksanteri Paakkinen <aleksanteri.paakkinen@gmail.com>
2018-07-18 15:10:45 +0200

    Change to 2d to ease plotting, fix integral

--- generator.py
+++ generator.py
@@ -9,7 +9,7 @@ from losses import regression_loss, generator_loss, jacobian, integral_loss
 from custom_functions import custom_tanh, doublegaussian, triplegaussian, integrate, tf_integrate, cauchy
 from logdet import logdet
 
-dist_dim = 4
+dist_dim = 2
 
 class RegressiveDNN():
     def __init__(self, dim):
@@ -53,7 +53,7 @@ class GenerativeDNN():
 h = RegressiveDNN(dist_dim)
 
 normal = tf.distributions.Uniform(-1.0, 1.0)
-generative = GenerativeDNN(dist_dim, normal, custom_tanh)#tf.keras.activations.tanh)#
+generative = GenerativeDNN(dist_dim, normal, tf.keras.activations.tanh)#custom_tanh)#
 gen_out = generative.output
 gen_in  = generative.input
 gen_grad = generative.abs_grad
@@ -70,8 +70,8 @@ hG_out = hG.output
 f = triplegaussian
 
 
-reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2, patience=5, min_lr=0.001)
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=2)
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.2, patience=5, min_lr=0.0001)
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 
 saver = tf.train.Saver()
 plot = False
@@ -92,14 +92,14 @@ with tf.Session() as sess:
         print("Loading regression from file")
         h.nn.load_weights(reg_file)
 
-    diagonals = np.reshape(np.repeat(np.arange(0.0, 1.0, 0.01), dist_dim), (-1,4))
+    diagonals = np.reshape(np.repeat(np.arange(0.0, 1.0, 0.01), dist_dim), (-1,dist_dim))
     plt.plot(np.arange(0.0, 1.0, 0.01), np.exp(h.nn.predict(diagonals)), '.r',
              np.arange(0.0, 1.0, 0.01), tf.keras.backend.eval(f(diagonals)), '.g')
     plt.show()
 
 
     h_G_z = hG_out #sess.run(reg_out, {reg_in: z})
-    integral_f = tf.Variable(sess.run(tf_integrate(tf.exp(h.output), 1), {h.input: xs}), trainable=False)
+    integral_f = tf.Variable(sess.run(tf_integrate(tf.exp(h.output), 1.0), {h.input: xs}), trainable=False)
     print(tf.keras.backend.eval(integral_f))
     integral_h_G_z = tf_integrate(h.output, 1.0)
     p_z = tf.reduce_prod(normal.prob(gen_in), (-1), keepdims=True)
@@ -115,7 +115,7 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     h.nn.load_weights(reg_file)
 
-    for e in range(1,50):
+    for e in range(1,5):
 
         batches = zip(np.reshape(zs, (-1, 1*512, dist_dim)), np.reshape(probs, (-1, 1*512, dist_dim)))
 
@@ -141,7 +141,16 @@ with tf.Session() as sess:
             plt.show()
 
     for cycle in range(1,10):
-        n, x, _ = plt.hist(sess.run(gen_out, {gen_in: x}).flatten(), 50, density=0.00311)
+        if dist_dim == 2:
+            xs = np.arange(-1.0, 1.0, 0.025)
+            ys = np.arange(-1.0, 1.0, 0.025)
+            X, Y = np.meshgrid(xs, ys)
+            Z = np.reshape(sess.run(gen_dst, {gen_in: np.array(list(zip(X.flatten(), Y.flatten()))).reshape([-1,2])}), (80,80))
+            plt.contour(X, Y, Z)
+            plt.show()
+            plt.matshow(Z)
+            plt.show()
+        n, x, _ = plt.hist(sess.run(gen_out, {gen_in: x}), 50, density=0.00311)
         plt.plot(x, sess.run(f(np.reshape(x, (-1,1)))).flatten())
         plt.show()
         zs = tf.keras.backend.eval(tf.reshape(normal.sample((5120*64)),(-1,dist_dim)))
@@ -152,14 +161,14 @@ with tf.Session() as sess:
         h.nn.fit(xs, correct, batch_size=int(5120/2), epochs=64, verbose=1
               ,callbacks=[reduce_lr, early_stopping])
 
-        diagonals = np.reshape(np.repeat(np.arange(0.0, 1.0, 0.01), dist_dim), (-1,4))
+        diagonals = np.reshape(np.repeat(np.arange(0.0, 1.0, 0.01), dist_dim), (-1,dist_dim))
         plt.plot(np.arange(0.0, 1.0, 0.01), np.exp(h.nn.predict(diagonals)), '.r',
                 np.arange(0.0, 1.0, 0.01), tf.keras.backend.eval(f(diagonals)), '.g')
         plt.show()
 
-        sess.run(integral_f.assign(sess.run(tf_integrate(tf.exp(h.output), 1), {h.input: xs})))
+        sess.run(integral_f.assign(sess.run(tf_integrate(tf.exp(hG_out), gen_dst), {hG_in: zs, gen_in: zs})))
         print("integral_f: ", sess.run(integral_f))
-        for e in range(1,15):
+        for e in range(1,45):
 
             batches = zip(np.reshape(zs, (-1, 10*512, dist_dim)), np.reshape(probs, (-1, 10*512, dist_dim)))
 
