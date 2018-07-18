d3afed096fbf3a789e594bc4e7c5dd9330df75c4
Aleksanteri Paakkinen <aleksanteri.paakkinen@gmail.com>
2018-07-18 16:20:38 +0200

    Fix wrong output dimension

--- custom_functions.py
+++ custom_functions.py
@@ -30,7 +30,7 @@ def doublegaussian(x):#(batch,dist_dim)
     dim_f = tf.cast(dim, tf.float32)
     c = 0.5*tf.pow(1/(a*math.sqrt(2*math.pi)),dim_f)
     rest = tf.exp(-tf.reduce_sum((x-1/3)*(x-1/3)/(2*a*a),1))+tf.exp(-tf.reduce_sum((x-2/3)*(x-2/3)/(2*a*a),1))
-    return tf.reshape(c*rest,(-1,dim))
+    return tf.reshape(c*rest,(-1,1))
 
 def triplegaussian(x):#(batch,dist_dim)
     x = tf.convert_to_tensor(x,dtype=tf.float32)
